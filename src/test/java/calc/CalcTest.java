1692b2786b78c73c57a3bc5f200448fc64089868
unknown <mshurin@P000113.playtika.local>
2014-10-13 12:36:00 +0300

    ttt

--- src/test/java/calc/CalcTest.java
+++ src/test/java/calc/CalcTest.java
@@ -9,6 +9,6 @@ public class CalcTest {
 	@Test
 	public void test() {
 		Calc calc = new Calc();
-		Assert.assertEquals(calc.add(2, 2), 4, 0.001);   
+		Assert.assertEquals(calc.add(2, 2), 4, 0.001);    
 	}
 }
